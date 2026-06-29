1bcf14a19f18744da1120429016bfe96c3f103aa
Mahesh <mahesh.neerati@kfintech.com>
2026-06-29 12:27:53 +0530

    config

--- src/api/axiosInstance.ts
+++ src/api/axiosInstance.ts
@@ -2,9 +2,9 @@ import axios, { InternalAxiosRequestConfig } from 'axios';
 
 const axiosInstance = axios.create({
   baseURL: 'http://192.168.0.121:5001/api',
-  headers: {
-    'Content-Type': 'application/json',
-  },
+  // headers: {
+  //   'Content-Type': 'application/json',
+  // },
 });
 
 axiosInstance.interceptors.request.use(
